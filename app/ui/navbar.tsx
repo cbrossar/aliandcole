54a0ac144d6b3a22602b4671eeb903cc76f05e87
Cole Brossart <cole.brossart@gmail.com>
2025-04-04 17:24:26 -0400

    tailwind

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <div>
+        <div className="flex justify-between items-center bg-[#edede4] text-black p-4 px-80 font-linden">
             <Link href="/">Home</Link>
             <Link href="/schedule">Schedule</Link>
             <Link href="/travel">Travel</Link>
             <Link href="/things-to-do">Things To Do</Link>
         </div>
-    );
-  }
+    );  
+}
   
