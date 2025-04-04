17056442bd912c9c62a6c68a093d218190feba8d
Cole Brossart <cole.brossart@gmail.com>
2025-04-04 18:01:24 -0400

    mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
             alt="Ciragan Palace" 
             width={1920}
             height={1080}
-            className="w-full"
+            className="w-full h-[50vh] object-cover"
           />
-          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[3.25rem] leading-[3.25rem] text-white font-['HeleneHess-Regular','fallback-1','fallback-2','fallback-3',serif]">
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl md:text-[3.25rem] leading-tight md:leading-[3.25rem] text-white font-['HeleneHess-Regular','fallback-1','fallback-2','fallback-3',serif]">
             Ali + Cole
           </h1>
         </div>
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <div className="flex justify-between items-center bg-[#edede4] text-black p-4 px-80 font-linden">
+        <div className="flex flex-wrap justify-center md:justify-between items-center bg-[#edede4] text-black p-4 px-4 md:px-8 lg:px-20 font-linden gap-4">
             <Link href="/">Home</Link>
             <Link href="/schedule">Schedule</Link>
             <Link href="/travel">Travel</Link>
