1ba2565b7fe84b359c1760277606588cd6cd1a90
Cole Brossart <cole.brossart@gmail.com>
2025-06-11 00:49:04 -0400

    a start

--- app/how-we-met/page.tsx
+++ app/how-we-met/page.tsx
@@ -1,7 +1,18 @@
-export default function ThingsToDo() {
+import Image from 'next/image';
+
+export default function HowWeMet() {
   return (
-      <div className="w-full flex flex-col items-center">
-        <h1 className="text-4xl font-bold mb-8">Things To Do</h1>
+    <div className="relative w-full h-screen">
+      <Image
+        src="/images/how-we-met/popsicle.jpg"
+        alt="How we met - couple sharing a popsicle"
+        fill
+        className="object-cover"
+        priority
+      />
+      <div className="absolute bottom-8 left-8">
+        <h1 className="text-white text-6xl font-light">How we met</h1>
       </div>
+    </div>
   );
 } 
