baa6ab5bd3062eaa6b60e16a2dca288c3e860311
Cole Brossart <cole.brossart@gmail.com>
2025-06-11 00:55:23 -0400

    yep

--- app/how-we-met/page.tsx
+++ app/how-we-met/page.tsx
@@ -19,7 +19,7 @@ export default function HowWeMet() {
       </div>
       <div className="px-8 pb-16">
         <p className="text-right text-lg leading-relaxed max-w-2xl ml-auto">
-          We both went to USC—Cole studied computer science, and Ali was in the school of medicine. Night of the UCLA game, we shared an uber to a dance party in Silver Lake. The stamps inked across our wrists read, "Be Dirty, Dance Clean." And that we did. The rest is history.
+          We both went to USC—Cole studied computer science, and Ali was in the school of medicine. Night of the UCLA game, we shared an uber to a dance party in Silver Lake. The stamps inked across our wrists read, &ldquo;Be Dirty, Dance Clean.&rdquo; And that we did. The rest is history.
         </p>
       </div>
     </div>
