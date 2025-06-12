ae21e62132baee1798a20c6fce1492d9564c8657
Cole Brossart <cole.brossart@gmail.com>
2025-06-12 00:50:13 -0400

    spotify

--- app/how-we-met/page.tsx
+++ app/how-we-met/page.tsx
@@ -22,6 +22,20 @@ export default function HowWeMet() {
           We both went to USC—Cole studied computer science, and Ali was in the school of medicine. Night of the UCLA game, we shared an uber to a dance party in Silver Lake. The stamps inked across our wrists read, &ldquo;Be Dirty, Dance Clean.&rdquo; And that we did. The rest is history.
         </p>
       </div>
+      {/* Spotify Player - order-3 on both mobile and desktop */}
+      <div className="flex justify-center col-span-1 lg:col-span-6 order-3 lg:order-3">
+            <div className="w-full max-w-sm lg:max-w-2xl">
+              <iframe 
+                style={{borderRadius:"12px"}} 
+                src="https://open.spotify.com/embed/track/2cmRpmO04TLaKPzmAzySYZ" 
+                width="100%" 
+                height="80" 
+                frameBorder="0" 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="lazy">
+              </iframe>
+            </div>
+          </div>
     </div>
   );
 } 
