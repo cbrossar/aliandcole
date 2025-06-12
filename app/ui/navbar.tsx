58078ddbc40faff92309411d5cc1aedbcc448bfb
Cole Brossart <cole.brossart@gmail.com>
2025-06-12 00:35:15 -0400

    UP FONT

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -37,7 +37,7 @@ export default function NavBar() {
 
     return (
         <div className="fixed top-0 left-0 right-0 z-50">
-            <div className="flex items-center justify-between bg-[rgb(248,245,239)] text-black p-4 px-4 md:px-8 lg:px-[42px] lg:py-[22px] shadow-lg">
+            <div className="flex items-center justify-between bg-[rgb(248,245,239)] text-black p-4 px-4 md:px-8 lg:px-[42px] lg:py-[22px] shadow-lg text-base md:text-lg">
                 <Link href="/" className={`${styles.title}`}>
                     Meet us at the palace
                 </Link>
