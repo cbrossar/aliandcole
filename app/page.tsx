e724bb66c8179b8f831bc489c5ee912eef106cc4
Cole Brossart <cole.brossart@gmail.com>
2025-07-06 10:08:09 -0400

    woop

--- app/page.tsx
+++ app/page.tsx
@@ -21,13 +21,12 @@ export default function Home() {
   return (
     <>
       <div className="w-full min-h-screen px-6 md:px-8 lg:px-12 pt-8 md:pt-12 lg:pt-16">
-        <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-1 lg:grid-cols-6 gap-1 lg:gap-16 min-h-[80vh] pt-8 lg:pt-12">
             {/* Text content - order-1 on mobile, order-1 on desktop */}
             <div className="flex flex-col justify-start space-y-0 lg:space-y-8 pt-4 pb-0 lg:pt-36 col-span-1 lg:col-span-2 order-1 lg:order-1">
               <FadeInOnScroll direction="left" delay={200}>
                 <h1 className="text-5xl md:text-5xl lg:text-6xl xl:text-7xl leading-tight text-[#333333] font-['Alice',serif]">
-                  Celebrating Ali & Cole
+                  Celebrating<br/>Ali & Cole
                 </h1>
               </FadeInOnScroll>
             </div>
@@ -49,7 +48,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </div>
 
       {/* New section with green background */}
       <div className="w-full py-24 md:py-32 lg:py-40" style={{ backgroundColor: 'rgb(142, 155, 142)' }}>
