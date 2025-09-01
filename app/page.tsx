31342a7c7cb891633b3b14f95515fa8407eb3847
Cole Brossart <cole.brossart@gmail.com>
2025-09-01 12:52:28 -0700

    build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import RsvpPopup from "./ui/rsvp-popup";
 import { FadeInOnScroll } from "./components/FadeInOnScroll";
